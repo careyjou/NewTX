6db7411f7dec24d3b18ebe9cf55ff49cca8ad182
yoshi <yoshijava@gmail.com>
2016-03-05 22:48:17 +0800

    Combine Day K and 60 K. Buy/Sell only if both of them are up/down. However, withdraw when the price meets 60K's.

--- src/Strategy.py
+++ src/Strategy.py
@@ -1,17 +1,31 @@
+lot = 0
+lot_Limit = 1
 
+def buy(price):
+    if buy_api() == True:
+        lot +=1
 
-if K_Day is up:
-    if empty():
-        if buy_or_not(K_60):
-            buy(K_60)
-    else:
-        if withdraw_or_not(K_60):
-            withdraw(K_60)
+def sell(price):
+    if sell_api() == True:
+        lot +=1
 
-elif K_Day is down:
-    if empty():
-        if sell_or_not(K_60):
-            sell(K_60)
-    else:
-        if withdraw_or_not(K_60):
-            withdraw(K_60)
+def monitor_loop():
+    global lot
+    global lot_limit
+
+    while True:
+        if lot < lot_limit:
+            if is_up_trend(K_Day) and is_up_trend(K_60):
+                buy(K_60)
+            elif is_down_trend(K_Day) and is_down_trend(K_60)
+                sell(K_60)
+
+        elif lot > 0:
+            if withdraw_or_not(K_60):
+                withdraw(K_60)
+
+        ch = 0xFF & cv2.waitKey(5)
+        if ch == 27:
+            break
+
+monitor_loop()
