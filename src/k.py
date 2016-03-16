f4d1d5d9797950fbef44f88775b0d30964d9b451
yoshi <yoshijava@gmail.com>
2016-03-16 23:04:42 +0800

    60k related codes using dict.

--- src/k.py
+++ src/k.py
@@ -1,11 +1,16 @@
 ''' all operations related to k bar is put here '''
+from time import strftime
+import mtx
+
+time_price = dict()
 
 # TODO: FOR TESTING
 TEST_PRICE = 8500
 
 def update_k_60():
-    # TODO
-    return TEST_PRICE
+    curr_time = strftime("%Y%m%d%H%M")
+    if curr_time.endswith("45"):
+        time_price[curr_time] = mtx.get_price()
 
 def update_k_day():
     # TODO
@@ -18,3 +23,6 @@ def k_day_trend():
 def k_60_trend():
     # TODO
     return 1
+
+if __name__ == '__main__':
+    update_k_60()
--- src/main.py
+++ src/main.py
@@ -7,15 +7,16 @@ import sys, time, os
 def monitor_loop():
     try:
         while True:
-            k_60 = update_k_60()
+            update_k_day()
+            update_k_60()
             lot = g.lot
 
             if abs(lot) > 0 and withdraw_or_not(k_60):
                 withdraw(k_60)
             elif abs(lot) < lot_limit:
-                if k_60_trend() == 1:
+                if k_day_trend() == 1 and k_60_trend() == 1:
                     buy(k_60)
-                elif k_60_trend() == -1:
+                elif k_day_trend() == -1 and k_60_trend() == -1:
                     sell(k_60)
                 else:
                     # TODO: What to do? Cool down?
