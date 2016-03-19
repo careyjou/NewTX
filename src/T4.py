94204f8f9bee8bb5cb747c183f077cec2bcb83f7
Emerson Huang <emersonhtc@gmail.com>
2016-03-19 19:54:00 +0800

    1. Add firebase support  2. fix error if no T4 backend support

--- src/T4.py
+++ src/T4.py
@@ -5,49 +5,57 @@ from py4j.java_gateway import JavaGateway
 import time
 
 gateway = JavaGateway()
-instance = gateway.entry_point.getInstance()
+try:
+	instance = gateway.entry_point.getInstance()
+except:
+	instance = None
+	print "T4 unavialable! run at test mode!"
 
 def buy_api(price):
-    old_lot = query_lot()
-    print 'old lot = ' + str(old_lot)
-    ret = instance.buy(price,"1")
-    if ret == None:
-        return False
+	old_lot = query_lot()
+	print 'old lot = ' + str(old_lot)
+	ret = instance.buy(price,"1")
+	if ret == None:
+		return False
 
-    new_lot = query_lot()
-    print 'new lot = ' + str(new_lot)
-    if new_lot < (old_lot + 1):
-        return False
-    else:
-        return True
+	new_lot = query_lot()
+	print 'new lot = ' + str(new_lot)
+	if new_lot < (old_lot + 1):
+		return False
+	else:
+		return True
 
 def sell_api(price):
-    old_lot = query_lot()
-    print 'old lot = ' + str(old_lot)
-    ret = instance.sell(price,"1")
-    if ret == None:
-        return False
+	old_lot = query_lot()
+	print 'old lot = ' + str(old_lot)
+	ret = instance.sell(price,"1")
+	if ret == None:
+		return False
 
-    new_lot = query_lot()
-    print 'new lot = ' + str(new_lot)
-    if new_lot < (old_lot + 1):
-        return False
-    else:
-        return True
+	new_lot = query_lot()
+	print 'new lot = ' + str(new_lot)
+	if new_lot < (old_lot + 1):
+		return False
+	else:
+		return True
 
 def query_lot():
-    ret = query_unsettled()
-    return ret[11]
+	ret = query_unsettled()
+	if ret:
+		return ret[11]
 
 def query_unsettled():
-    ret = instance.queryUnsettled().encode("big5")
-    while ret.find('Error') != -1:
-        time.sleep(5)
-        ret = instance.queryUnsettled().encode("big5")
+	if instance:
+		ret = instance.queryUnsettled().encode("big5")
+		while ret.find('Error') != -1:
+			time.sleep(5)
+			ret = instance.queryUnsettled().encode("big5")
 
-    if ret.find("MTX") == -1:
-        # TODO: What will be returned if the # of lot = 0?
-        return 0
-    else:
-        list = ret.split()
-        return list
+		if ret.find("MTX") == -1:
+			# TODO: What will be returned if the # of lot = 0?
+			return 0
+		else:
+			list = ret.split()
+			return list
+	else:
+		return None
--- src/k.py
+++ src/k.py
@@ -3,6 +3,9 @@ import os
 import mtx
 import json
 from datetime import datetime, timedelta
+import time
+
+import firebase
 
 time_price = dict()
 yesterday_time_price = dict()
@@ -12,37 +15,56 @@ legal_hour = map(str,range(8,14))
 TEST_PRICE = 8500
 
 def update_k_60():
-    curr_time = strftime("%H%M")
-    curr_date = strftime("%Y%m%d")
-    if curr_time.endswith("45") and curr_time[0:2] in legal_hour:
-        time_price[curr_time] = mtx.get_price()
-        outfile = '../history/' + current_date + '.json'
-        with open(outfile, 'w') as outfile:
-            json.dump(data, outfile)
+	curr_time = time.strftime("%H%M")
+	curr_date = time.strftime("%Y%m%d")
+	if curr_time.endswith("45") and curr_time[0:2] in legal_hour:
+		time_price[curr_time] = mtx.get_price()
+		outfile = '../history/' + current_date + '.json'
+		with open(outfile, 'w') as outfile:
+			json.dump(data, outfile)
 
 def update_k_day():
+	yesterday_date = datetime.now() - timedelta(1)
+	yesterday_str = yesterday_date.strftime("%Y%m%d")
+	infile = '../history/' + yesterday_str + '.json'
+	while os.path.isfile(infile) != True:
+		yesterday_date = yesterday_date - timedelta(1)
+		yesterday_str = yesterday_date.strftime("%Y%m%d")
+		infile = '../history/' + yesterday_str + '.json'
 
-    yesterday_date = datetime.now() - timedelta(1)
-    yesterday_str = yesterday_date.strftime("%Y%m%d")
-    infile = '../history/' + yesterday_str + '.json'
-    while os.path.isfile(infile) != True:
-        yesterday_date = yesterday_date - timedelta(1)
-        yesterday_str = yesterday_date.strftime("%Y%m%d")
-        infile = '../history/' + yesterday_str + '.json'
+	print yesterday_date
 
-    print yesterday_date
+	with open(infile, 'r') as infile:
+		yesterday_time_price = json.loads(infile.read())
+	return yesterday_time_price
 
-    with open(infile, 'r') as infile:
-        yesterday_time_price = json.loads(infile.read())
-    return yesterday_time_price
+def update_k_day1():
+	date_delta=1
+	MONDAY, SUNDAY=0 ,6
+
+	MYURL  = 'https://boiling-heat-5294.firebaseio.com/'
+	EMAIL  = 'emersonhtc@gmail.com'
+	SECRET = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P'
+
+	authentication = firebase.FirebaseAuthentication(SECRET,EMAIL, True, True)
+	fb = firebase.FirebaseApplication(MYURL, authentication)
+	
+	today = datetime.now().date()
+	date_delta = 3 if today.weekday() == MONDAY else 1
+	date_delta = 2 if today.weekday() == SUNDAY else 1
+
+	last_trade_date = today - timedelta(date_delta)
+	access_str = str(last_trade_date).replace('-','/')
+	return fb.get(access_str,None)
+	
 
 def k_day_trend():
-    # TODO
-    return 1
+	# TODO
+	return 1
 
 def k_60_trend():
-    # TODO
-    return 1
+	# TODO
+	return 1
 
 if __name__ == '__main__':
-    print update_k_day()
+	print update_k_day1()
