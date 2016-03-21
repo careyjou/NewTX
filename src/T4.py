bad4bebabc39a2c5b16a75fd68f44cacff84bd71
yoshi <yoshijava@gmail.com>
2016-03-21 20:36:59 +0800

    tabs to space.

--- src/T4.py
+++ src/T4.py
@@ -5,56 +5,56 @@ import time
 
 gateway = JavaGateway()
 try:
-	instance = gateway.entry_point.getInstance()
+    instance = gateway.entry_point.getInstance()
 except:
-	instance = None
-	print "T4 unavialable! run at test mode!"
+    instance = None
+    print "T4 unavialable! run at test mode!"
 
 def buy_api(price):
-	old_lot = query_lot()
-	print 'old lot = ' + str(old_lot)
-	ret = instance.buy(price,"1")
-	if ret == None:
-		return False
+    old_lot = query_lot()
+    print 'old lot = ' + str(old_lot)
+    ret = instance.buy(price,"1")
+    if ret == None:
+        return False
 
-	new_lot = query_lot()
-	print 'new lot = ' + str(new_lot)
-	if new_lot < (old_lot + 1):
-		return False
-	else:
-		return True
+    new_lot = query_lot()
+    print 'new lot = ' + str(new_lot)
+    if new_lot < (old_lot + 1):
+        return False
+    else:
+        return True
 
 def sell_api(price):
-	old_lot = query_lot()
-	print 'old lot = ' + str(old_lot)
-	ret = instance.sell(price,"1")
-	if ret == None:
-		return False
+    old_lot = query_lot()
+    print 'old lot = ' + str(old_lot)
+    ret = instance.sell(price,"1")
+    if ret == None:
+        return False
 
-	new_lot = query_lot()
-	print 'new lot = ' + str(new_lot)
-	if new_lot < (old_lot + 1):
-		return False
-	else:
-		return True
+    new_lot = query_lot()
+    print 'new lot = ' + str(new_lot)
+    if new_lot < (old_lot + 1):
+        return False
+    else:
+        return True
 
 def query_lot():
-	ret = query_unsettled()
-	if ret:
-		return ret[11]
+    ret = query_unsettled()
+    if ret:
+        return ret[11]
 
 def query_unsettled():
-	if instance:
-		ret = instance.queryUnsettled().encode("big5")
-		while ret.find('Error') != -1:
-			time.sleep(5)
-			ret = instance.queryUnsettled().encode("big5")
+    if instance:
+        ret = instance.queryUnsettled().encode("big5")
+        while ret.find('Error') != -1:
+            time.sleep(5)
+            ret = instance.queryUnsettled().encode("big5")
 
-		if ret.find("MTX") == -1:
-			# TODO: What will be returned if the # of lot = 0?
-			return 0
-		else:
-			list = ret.split()
-			return list
-	else:
-		return None
+        if ret.find("MTX") == -1:
+            # TODO: What will be returned if the # of lot = 0?
+            return 0
+        else:
+            list = ret.split()
+            return list
+    else:
+        return None
