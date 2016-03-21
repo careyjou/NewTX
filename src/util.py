d997c4b7abc4e37ff321c0fb4517fdb326ff1db0
Yoshi <yoshijava@gmail.com>
2016-03-21 14:37:16 +0800

    Added a firebase utility class for simplyfing I/O operations.

--- src/util.py
+++ src/util.py
@@ -1,16 +1,39 @@
 import firebase
 import json
 
-def print_json(rest_url):
-    MYURL  = 'https://boiling-heat-5294.firebaseio.com/'
-    EMAIL  = 'emersonhtc@gmail.com'
-    SECRET = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P'
+class Util:
+    url = None
+    secret_key = None
+    email = None
+    fb = None
+    authentication = None
 
-    authentication = firebase.FirebaseAuthentication(SECRET,EMAIL, True, True)
-    fb = firebase.FirebaseApplication(MYURL, authentication)
-    result = fb.get(rest_url, None)
-    print json.dumps(result, indent=4, sort_keys=True)
+    def __init__(self,url,secret_key,email):
+        self.authentication = firebase.FirebaseAuthentication(secret_key,email, True, True)
+        self.fb = firebase.FirebaseApplication(url, self.authentication)
+        self.url = url
+
+    def __str__(self):
+        result = self.fb.get('/', None)
+        return json.dumps(result, indent=4, sort_keys=True)
+
+    def print_json(self, rest_url):
+        result = self.fb.get(rest_url, None)
+        print json.dumps(result, indent=4, sort_keys=True)
+
+    def delete(self,key):
+        ret = self.fb.delete(self.url,key)
+
+    def put(self,key,value):
+        ret = self.fb.put(self.url,key,value)
+        if ret == value:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-    print_json("/2016/")
-    print_json("/2016/03/")
+    url = 'https://boiling-heat-5294.firebaseio.com/'
+    secret_key = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P'
+    email = 'emersonhtc@gmail.com'
+    instance = Util(url,secret_key,email)
+    print str(instance)
