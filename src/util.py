08063b1ce4fe6758eda20c68b79fd2967efc3b1a
Yoshi <yoshijava@gmail.com>
2016-03-21 17:52:42 +0800

    refactory

--- src/util.py
+++ src/util.py
@@ -2,13 +2,13 @@ import firebase
 import json
 
 class Util:
-    url = 'https://boiling-heat-5294.firebaseio.com/'
-    secret_key = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P'
-    email = 'emersonhtc@gmail.com'
+
     fb = None
     authentication = None
 
-    def __init__(self,url,secret_key,email):
+    def __init__(self, url = 'https://boiling-heat-5294.firebaseio.com/',
+                        secret_key = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P',
+                        email = 'emersonhtc@gmail.com'):
         self.authentication = firebase.FirebaseAuthentication(secret_key,email, True, True)
         self.fb = firebase.FirebaseApplication(url, self.authentication)
         self.url = url
@@ -21,6 +21,10 @@ class Util:
         result = self.fb.get(rest_url, None)
         print json.dumps(result, indent=4, sort_keys=True)
 
+    def get(self, rest_url):
+        result = self.fb.get(rest_url, None)
+        return result
+
     def delete(self,key):
         ret = self.fb.delete(self.url,key)
 
@@ -32,8 +36,5 @@ class Util:
             return False
 
 if __name__ == '__main__':
-    url = 'https://boiling-heat-5294.firebaseio.com/'
-    secret_key = '5zF8T37NlZB1HBHkb9T1zAZ6c2cPbXiXlzP5yJ8P'
-    email = 'emersonhtc@gmail.com'
-    instance = Util(url,secret_key,email)
-    print str(instance)
+    instance = Util()
+    print instance.get('/2016/03')
